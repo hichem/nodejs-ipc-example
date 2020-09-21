85f6e7149122251587e219a7fdaa10c9ce40803a
Hichem BOUSSETTA <hichem.boussetta@ratpsmartsystems.com>
2020-09-21 12:02:54 +0200

    added missing call to socket send

--- zmq-client/index.js
+++ zmq-client/index.js
@@ -8,6 +8,10 @@ async function run() {
   sock.connect("ipc:///tmp/app.world");
   console.log("Client bound to ipc socket");
 
+  // Send message
+  await sock.send("Hello World 6");
+
+  // Receive messages asynchronously
   sock.on('message', function(reply) {
     console.log('Received Message:', reply.toString());
   });
