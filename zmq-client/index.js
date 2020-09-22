e1f6c029d2b77287d03ebf959cb4740f9d1ae9ba
Hichem BOUSSETTA <hichem.boussetta@ratpsmartsystems.com>
2020-09-22 11:38:44 +0200

    [TPV-470] added comments & added vagrant files

--- zmq-client/index.js
+++ zmq-client/index.js
@@ -1,21 +1,39 @@
 const zmq = require("zeromq");
 
+// Refer to https://github.com/zeromq/zeromq.js/#clientjs
+
 async function run() {
-  sock = zmq.socket("req");
-  sock.identity = "zmq-client" + process.pid;
 
-  //sock.bind("ipc:///tmp/app.world");
+    // Construct for zmq v5
+    sock = zmq.socket("req");
+    
+    // Construct for zmq v6
+    //const sock = new zmq.Request
 
-  sock.connect("ipc:///tmp/app.world");
-  console.log("Client bound to ipc socket");
+    // Assign identity  to socket
+    sock.identity = "zmq-client-" + process.pid;
 
-  // Send message
-  await sock.send("Hello World 6");
+    //sock.bind("ipc:///tmp/app.world");
 
-  // Receive messages asynchronously
-  sock.on('message', function(data) {
-    console.log('[' + sock.identity + '] ' + 'Received Message: ' + data.toString());
-  });
+    try {
+        sock.connect("ipc:///tmp/app.world");
+        console.log("Client bound to ipc socket");
+
+        // Send message
+        await sock.send("Hello World");
+
+        // Receive messages asynchronously
+        sock.on('message', function(data) {
+            console.log('[' + sock.identity + '] ' + 'Received Message: ' + data.toString());
+        });
+
+    } catch (err) {
+        // Display connection error
+        console.log("Connect Error: " + err);
+
+        // Exit
+        process.exit(1);
+    }
   
 }
 
--- zmq-server/index.js
+++ zmq-server/index.js
@@ -1,23 +1,34 @@
 const zmq = require("zeromq");
 
+// Refer to https://github.com/zeromq/zeromq.js/#serverjs
+
 async function run() {
-  sock = zmq.socket("rep");
 
-  sock.identity = "zmq-server" + process.pid;
-  sock.bind("ipc:///tmp/app.world");
+    // Construct for zmq v5
+     sock = zmq.socket("rep");
 
-  //sock.connect("ipc:///tmp/app.world");
-  //console.log("Client bound to ipc socket");
+    // Construct for zmq v6
+    // const sock = new zmq.Reply
 
-  // Send message
-  //await sock.send("Hello World 6");
+    // Assign identity to the server socket
+    sock.identity = "zmq-server-" + process.pid;
 
-  // Receive messages asynchronously
-  sock.on('message', function(data) {
-    console.log('[' + sock.identity + '] ' + 'Received Message: ' + data.toString());
+    // Create the ipc socket / pipe
+    sock.bind("ipc:///tmp/app.world");
 
-    sock.send(data.toString());
-  });
+    // Server socket does not need to connect. Only bind is required
+    //sock.connect("ipc:///tmp/app.world");
+    //console.log("bound to ipc socket");
+
+    // Receive messages asynchronously
+    sock.on('message', function(data) {
+
+        // Display message received from client
+        console.log('[' + sock.identity + '] ' + 'Received Message: ' + data.toString());
+
+        // Return same message to client (loopback)
+        sock.send(data.toString());
+    });
   
 }
 
